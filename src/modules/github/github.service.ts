c1db5dd3e281a69e24ce407e1a16b26f797a72f0
cxvint <cxvint@gmail.com>
2021-12-02 19:22:36 +0300

    test final

--- src/modules/github/github.service.ts
+++ src/modules/github/github.service.ts
@@ -1,34 +1,34 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
 export class GithubService {
-  constructor(private httpService: HttpService) {}
+  async getAllAction(
+    repoName: string,
+    branchName: string,
+    perPage = 25,
+    filter: string,
+  ): Promise<{ data: any[]; count: number }> {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/${repoName}/${branchName}/commits`,
+      { params: { per_page: perPage } },
+    );
+    if (!filter) {
+      return { data, count: data.length };
+    }
 
-   async getAllAction(repoName: string, branchName: string, perPage=25, filter: string ): Promise<{ data: any[]; count: number; }> {
-      
-      const {data} = await axios.get(`https://api.github.com/repos/${repoName}/${branchName}/commits`, {params: {per_page: perPage}});
-      if (!filter) {
-        return {data, count: data.lengh};
+    const filterData = data.map((commit) => {
+      if (filter === 'sha') {
+        return { sha: commit.sha };
       }
-      
-      const filterData = data.map((commit) => {
-    
-        if (filter === 'sha') {
-          return {sha:commit.sha}
-          }
 
-        if (filter === 'message') {
-          return {message:commit.commit.message}
-        }
-        return commit;
+      if (filter === 'message') {
+        return { message: commit.commit.message };
+      }
+      return commit;
     });
-    const count = filterData.lengh;
-    
-      return {count: count, data: filterData}
+    const count = filterData.length;
 
+    return { count: count, data: filterData };
   }
-
 }
-
