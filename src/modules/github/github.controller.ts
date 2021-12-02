807a5ff7ba3535dba4f26751abb3388cecbf3ef4
cxvint <cxvint@gmail.com>
2021-12-02 22:32:18 +0300

    Create Swagger

--- src/modules/github/github.controller.ts
+++ src/modules/github/github.controller.ts
@@ -1,18 +1,24 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { GithubService } from './github.service';
 
+@ApiTags('/api/github')
 @Controller('github')
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
   @Get('repository/:repoName/branch/:branchName/commits')
   async getAllAction(
-      @Param('repoName') repoName: string,
-      @Param('branchName') branchName : string,
-      @Query('per_page') perPage : number,
-      @Query('filter') filter: string
-  )
-      {
-    return this.githubService.getAllAction(repoName, branchName, perPage, filter);
+    @Param('repoName') repoName: string,
+    @Param('branchName') branchName: string,
+    @Query('filter') filter: string,
+    @Query('per_page') perPage: number,
+  ) {
+    return this.githubService.getAllAction(
+      repoName,
+      branchName,
+      filter,
+      perPage,
+    );
   }
 }
